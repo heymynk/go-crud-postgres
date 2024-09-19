abe94e7de0757061bba6676b4089cdfbc8d25939
Hey-Mynk <mail.contactmayank@gmail.com>
2024-09-19 10:21:17 +0530

    commit

--- controllers/employee_controller.go
+++ controllers/employee_controller.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// GetEmployees Function will fetch all employees
 func GetEmployees(c *fiber.Ctx) error {
 	var employees []models.Employee
 	if err := config.DB.Find(&employees).Error; err != nil {
@@ -15,6 +16,7 @@ func GetEmployees(c *fiber.Ctx) error {
 	return c.JSON(employees)
 }
 
+// GetEmployee Function will fetch employee using id
 func GetEmployee(c *fiber.Ctx) error {
 	id := c.Params("id")
 	var employee models.Employee
@@ -24,6 +26,7 @@ func GetEmployee(c *fiber.Ctx) error {
 	return c.JSON(employee)
 }
 
+// CreateEmployee Function will create new employee
 func CreateEmployee(c *fiber.Ctx) error {
 	var employee models.Employee
 	if err := c.BodyParser(&employee); err != nil {
@@ -35,6 +38,7 @@ func CreateEmployee(c *fiber.Ctx) error {
 	return c.JSON(employee)
 }
 
+// UpdateEmployee Function will update employee
 func UpdateEmployee(c *fiber.Ctx) error {
 	id := c.Params("id")
 	var employee models.Employee
@@ -50,6 +54,7 @@ func UpdateEmployee(c *fiber.Ctx) error {
 	return c.JSON(employee)
 }
 
+// DeleteEmployee Function will delete an existing employee
 func DeleteEmployee(c *fiber.Ctx) error {
 	id := c.Params("id")
 	if err := config.DB.Delete(&models.Employee{}, id).Error; err != nil {
